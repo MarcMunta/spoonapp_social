97e94128567f07fbb3d118fd71a243531aa849c6
Marc Muntané <mmclara1050@gmail.com>
2025-06-18 12:34:26 +0100

    Remove obsolete base_context context processor

--- backend/spoonapp_social/settings.py
+++ backend/spoonapp_social/settings.py
@@ -41,7 +41,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'social.context_processors.friend_requests_processor',
-                'social.views.base_context',
             ],
         },
     },

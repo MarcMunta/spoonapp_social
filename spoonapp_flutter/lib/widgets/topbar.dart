765ec342624aa93a310efca31dd9b1cb8ba96149
Marc Muntané <mmclara1050@gmail.com>
2025-07-04 20:27:08 +0100

    Redesign top bar with logo and navigation buttons

--- spoonapp_flutter/lib/widgets/topbar.dart
+++ spoonapp_flutter/lib/widgets/topbar.dart
@@ -1,58 +1,123 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../providers/user_provider.dart';
 
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   const TopBar({super.key, this.title = ''});
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(70);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      centerTitle: true,
-      backgroundColor: Colors.transparent,
-      leading: const Icon(Icons.restaurant_menu, color: Colors.white),
-      title: Image.asset(
-        'assets/images/spoonapp.png',
-        height: 45,
-        fit: BoxFit.contain,
-      ),
-      flexibleSpace: Container(
+    final width = MediaQuery.of(context).size.width;
+    final showNav = width > 600;
+    final showLogo = width > 350;
+
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: preferredSize.height,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xFFB46DDD), Color(0xFFD9A7C7)],
           ),
         ),
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.home, color: Colors.white),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: const Icon(Icons.add, color: Colors.white),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: const Icon(Icons.notifications, color: Colors.white),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: const Icon(Icons.restaurant, color: Colors.white),
-          onPressed: () {},
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 8.0),
-          child: Consumer<UserProvider>(
-            builder: (context, provider, _) => CircleAvatar(
-              backgroundImage: NetworkImage(provider.currentUser.profileImage),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            if (showNav)
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _NavButton(icon: Icons.home, onPressed: () {}),
+                  _NavButton(icon: Icons.add, onPressed: () {}),
+                  _NavButton(icon: Icons.notifications, onPressed: () {}),
+                  _NavButton(icon: Icons.restaurant, onPressed: () {}),
+                ],
+              ),
+            Positioned(
+              left: 8,
+              child: _MenuButton(
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+              ),
             ),
-          ),
-        )
-      ],
+            if (showLogo)
+              Positioned(
+                right: 8,
+                child: Image.asset(
+                  'assets/images/spoonapp.png',
+                  height: 45,
+                  fit: BoxFit.contain,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MenuButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _MenuButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: const Color(0xFFFFF0B3),
+      shape: const CircleBorder(),
+      child: InkWell(
+        customBorder: const CircleBorder(),
+        onTap: onPressed,
+        child: const Padding(
+          padding: EdgeInsets.all(8),
+          child: Icon(Icons.menu, color: Color(0xFF5D1049)),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavButton extends StatefulWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+  const _NavButton({required this.icon, required this.onPressed});
+
+  @override
+  State<_NavButton> createState() => _NavButtonState();
+}
+
+class _NavButtonState extends State<_NavButton> {
+  bool _hover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hover = true),
+      onExit: (_) => setState(() => _hover = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        margin: const EdgeInsets.symmetric(horizontal: 12),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFF0B3),
+          shape: BoxShape.circle,
+          boxShadow: _hover
+              ? const [
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 4,
+                      offset: Offset(0, 2))
+                ]
+              : null,
+        ),
+        child: IconButton(
+          icon: Icon(widget.icon, color: const Color(0xFF5D1049)),
+          onPressed: widget.onPressed,
+          splashRadius: 20,
+        ),
+      ),
     );
   }
 }
